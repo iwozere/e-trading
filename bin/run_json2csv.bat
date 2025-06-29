874a69a8d0508ee1efacb9c05bb35aab1f0b0e41
Alex Kosyrev <akossyrev@gmail.com>
2025-06-29 20:24:17 +0200

    xxx

--- bin/run_json2csv.bat
+++ bin/run_json2csv.bat
@@ -34,7 +34,7 @@ if errorlevel 1 (
 
 REM Run the JSON to CSV converter script
 echo Running JSON to CSV converter...
-python src\plotter\run_json2csv.py
+python src\optimizer\run_json2csv.py
 
 REM Check if script ran successfully
 if errorlevel 1 (
